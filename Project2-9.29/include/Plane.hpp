99a8473e726697400b10aa5a96ccec53f5274cc2
longjj <luojj26@mail2.sysu.edu.cn>
2016-10-05 23:21:38 +0800

    update the class Plane

--- Project2-9.29/include/Plane.hpp
+++ Project2-9.29/include/Plane.hpp
@@ -1,11 +1,16 @@
-#ifndef Plane_hpp
-#define Plane_hpp
+// write by Junjun on 10.4
+// modify by longj on 10.5
 
-#include <cstdio>
+#ifndef PLANE_HPP_
+#define PLANE_HPP_
+
 #include "enum.hpp"
 class Plane {
  public:
   Plane();
+  /**
+   * initial a plane with its number, time and status
+   */
   Plane(int flt, int time, Plane_status status);
   void refuse() const;
   void land(int time) const;
@@ -13,8 +18,8 @@ class Plane {
   int started() const;
 
  private:
-  int flt_num;
-  int clock_start;
-  Plane_status state;
+  int m_flt_num;
+  int m_clock_start;
+  Plane_status m_state;
 };
-#endif /* Plane_hpp */
+#endif
--- Project2-9.29/src/Plane.cpp
+++ Project2-9.29/src/Plane.cpp
@@ -1,49 +1,47 @@
+// write by Junjun on 10.4
+// modify by longj on 10.5
+
 #include "Plane.hpp"
 #include <iostream>
-using std::cout;
-using std::endl;
 
-Plane::Plane() {
-  flt_num = -1;
-  clock_start = -1;
-  state = null;
-}
+Plane::Plane() : m_flt_num(-1), m_clock_start(-1), m_state(null) {}
 
-Plane::Plane(int flt, int time, Plane_status status) {
-  flt_num = flt;
-  clock_start = time;
-  state = status;
-  cout << "Plane number" << flt << "ready to";
+/**
+ * initial a plane with its number, time and status
+ */
+Plane::Plane(int flt, int time, Plane_status status)
+    : m_flt_num(flt), m_clock_start(time), m_state(status) {
+  std::cout << "Plane number" << flt << "ready to";
   if (status == arriving)
-    cout << "land" << endl;
-  else
-    cout << "take off" << endl;
+    std::cout << "land" << std::endl;
+  else if (status == departing)
+    std::cout << "take off" << std::endl;
 }
 
 // processes a Plane wantting to use Runway, when Queue is full
 void Plane::refuse() const {
-  cout << "Plane number " << flt_num;
-  if (state == arriving)
-    cout << "directed to another airport" << endl;
-  else
-    cout << "told to try to takeoff again later" << endl;
+  std::cout << "Plane number" << m_flt_num;
+  if (m_state == arriving)
+    std::cout << "directed to another airport" << std::endl;
+  else if (m_state == departing)
+    std::cout << "told to try to takeoff again later" << std::endl;
 }
 
 // processes a Plane that is landding at the specified time
 void Plane::land(int time) const {
-  int wait = time - clock_start;
-  cout << time << ":Plane number " << flt_num << "landed after" << wait
-       << " time unit" << ((wait == 1) ? "" : "s") << " in the takeoff queue."
-       << endl;
+  int wait = time - m_clock_start;
+  std::cout << time << ":Plane number " << m_flt_num << "landed after" << wait
+            << " time unit" << ((wait == 1) ? "" : "s")
+            << " in the takeoff queue." << std::endl;
 }
 
 // processes a Plane that is taking off at the specified time
 void Plane::fly(int time) const {
-  int wait = time - clock_start;
-  cout << time << ":Plane number " << flt_num << " took off after" << wait
-       << " time unit" << ((wait == 1) ? "" : "s") << "in the takeoff queue."
-       << endl;
+  int wait = time - m_clock_start;
+  std::cout << time << ":Plane number " << m_flt_num << " took off after"
+            << wait << " time unit" << ((wait == 1) ? "" : "s")
+            << "in the takeoff queue." << std::endl;
 }
 
 // return the time that the Plane entered the airport system
-int Plane::started() const { return clock_start; }
+int Plane::started() const { return m_clock_start; }
