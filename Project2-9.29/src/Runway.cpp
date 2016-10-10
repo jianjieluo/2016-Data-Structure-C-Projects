77e60bf0742fe60f6fdf5cfe3092281c91426c2c
longjj <luojj26@mail2.sysu.edu.cn>
2016-10-11 01:20:37 +0800

    add a new command 'clearData' in the makefile

--- Project2-9.29/src/Runway.cpp
+++ Project2-9.29/src/Runway.cpp
@@ -1,6 +1,6 @@
 #include "Runway.hpp"
-#include <iostream>
 #include <fstream>
+#include <iostream>
 using std::cout;
 using std::endl;
 
@@ -32,17 +32,18 @@ Error_code Runway::can_land(const Plane& current) {
   return result;
 }
 
-// Post: If possible, the Plane current is added to the takeoff Queue; otherwise, an
+// Post: If possible, the Plane current is added to the takeoff Queue;
+// otherwise, an
 // Error_code of overflow is returned. The Runway statistics are update.
 // Uses: class Extended_queue.
-Error_code Runway::can_depart(const Plane &current) {
+Error_code Runway::can_depart(const Plane& current) {
   Error_code result;
   if (takeoffing.size() < queue_limit)
     result = takeoffing.push(current);
   else
     result = fail;
   num_takeoff_requests++;
-  if (result!=success)
+  if (result != success)
     num_takeoff_refused++;
   else
     num_takeoff_accepted++;
@@ -78,40 +79,44 @@ Runway_activity Runway::activity(int time, Plane& moving) {
 // Runwfstream.h: No such file or directory
 
 void Runway::shut_down(int time) const {
-    std::ofstream s("../data/summary.txt", std::ios_base::app);
-  if (!s) cout << 1233333;
-  s    << endl
-       << "Sumualation has conluded after " << time << " time units." << endl
-       << "Total number of planes processed \t\t\t"
-       << (num_land_requests + num_takeoff_requests) << endl
-       << "Total number of planes asking to land \t\t" << (num_land_requests)
-       << endl
-       << "Total number of planes asking to take off \t\t" << (num_takeoff_requests)
-       << endl
-       << "Total number of planes accepted for landing \t\t" << (num_land_accepted)
-       << endl
-       << "Total number of planes accepted for takeoff \t\t"
-       << (num_takeoff_accepted) << endl
-       << "Total number of planes refused for landing \t\t" << (num_land_refused)
-       << endl
-       << "Total number of planes refused for takeoff\t\t" << (num_takeoff_refused)
-       << endl
-       << "Total number of planes that landed\t\t\t" << (num_landings) << endl
-       << "Total number of planes that took off\t\t\t" << (num_takeoffs) << endl
-       << "Total number of planes that left in landing queue\t" << landing.size()
-       << endl
-       << "Total number of planes that left in takeoff queue \t"
-       << takeoffing.size() << endl;
-  s    << "Percentage of time runway idle \t\t\t"
-       << 100.0 * (((float)idle_time) / ((float)time)) << "% " << endl;
-  s    << "Average wait in landing queue \t\t\t\t"
-       << ((float)land_wait) / ((float)num_landings) << " time units" << endl;
-  s    << "Average wait in takeoff queue \t\t\t\t"
-       << ((float)takeoff_wait) / ((float)num_takeoffs) << " time units" << endl;
-  s    << "Average obserbed rate of planes wanting to land \t"
-       << ((float)num_land_requests) / ((float)time) << " per time unit" << endl;
-  s    << "Average obserbed rate of planes wanting to take off \t"
-       << ((float)num_takeoff_requests) / ((float)time) << " per time unit"
-       << endl;
-  s.close();
+  std::ofstream s("./data/summary.txt", std::ios_base::app);
+  if (!s) {
+    cout << "Can't open the data file";
+  } else {
+    s << endl
+      << "Sumualation has conluded after " << time << " time units." << endl
+      << "Total number of planes processed \t\t\t"
+      << (num_land_requests + num_takeoff_requests) << endl
+      << "Total number of planes asking to land \t\t" << (num_land_requests)
+      << endl
+      << "Total number of planes asking to take off \t\t"
+      << (num_takeoff_requests) << endl
+      << "Total number of planes accepted for landing \t\t"
+      << (num_land_accepted) << endl
+      << "Total number of planes accepted for takeoff \t\t"
+      << (num_takeoff_accepted) << endl
+      << "Total number of planes refused for landing \t\t" << (num_land_refused)
+      << endl
+      << "Total number of planes refused for takeoff\t\t"
+      << (num_takeoff_refused) << endl
+      << "Total number of planes that landed\t\t\t" << (num_landings) << endl
+      << "Total number of planes that took off\t\t\t" << (num_takeoffs) << endl
+      << "Total number of planes that left in landing queue\t" << landing.size()
+      << endl
+      << "Total number of planes that left in takeoff queue \t"
+      << takeoffing.size() << endl;
+    s << "Percentage of time runway idle \t\t\t"
+      << 100.0 * (((float)idle_time) / ((float)time)) << "% " << endl;
+    s << "Average wait in landing queue \t\t\t\t"
+      << ((float)land_wait) / ((float)num_landings) << " time units" << endl;
+    s << "Average wait in takeoff queue \t\t\t\t"
+      << ((float)takeoff_wait) / ((float)num_takeoffs) << " time units" << endl;
+    s << "Average obserbed rate of planes wanting to land \t"
+      << ((float)num_land_requests) / ((float)time) << " per time unit" << endl;
+    s << "Average obserbed rate of planes wanting to take off \t"
+      << ((float)num_takeoff_requests) / ((float)time) << " per time unit"
+      << endl;
+    s.close();
+    std::cout << "Write down to the data file successfully" << std::endl;
+  }
 }
