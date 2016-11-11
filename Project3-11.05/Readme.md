bae8a51a571c600dcd0df5a1cf308d73391c0c62
longjj <luojj26@mail2.sysu.edu.cn>
2016-11-12 01:26:56 +0800

    finish the framework of the Project3 family tree and the 1st version of the hpp files

---
+++ Project3-11.05/Readme.md
@@ -0,0 +1,7 @@
+> Please read the include files in the include/ and get to know what the functions for and prepare to start working
+
+> when set up the hpp, we can develop our own cpp files in our own dir in the Unit_test dir, you can use your own data temp files to check your codes
+
+> Please be ready to check your cpp files before the ddl with gtest or your own main files before you think your code can work correctly. One Bug for 2 yuan to set up our final happy food~
+
+> Maybe we can finish as a web app and display prettier with css and js. But that is just a dream now .
---
+++ Project3-11.05/include/familyTree.hpp
@@ -0,0 +1,47 @@
+#ifndef FAMILYTREE_HPP_
+#define FAMILYTREE_HPP_
+#include <functional>
+#include <vector>
+#include "treeNode.hpp"
+
+class familyTree {
+ private:
+  bool deleteNode(treeNode* t_root);
+  treeNode* searchPersonByName(treeNode* t_root, const std::string t_name);
+
+ public:
+  familyTree();
+
+  // You need to use the URL to get the infomation of the old data;
+  familyTree(const std::string data_path);
+  // remember to sync the data when destroy the tree;
+  ~familyTree();
+
+  // delete the current node and its subnodes.
+  bool deleteNode(const std::string t_name);
+  bool insertPersonNode(const std::string t_father_name);
+  bool resetPersonWifeInfo(const std::string t_name);
+
+  treeNode* searchPersonByName(const std::string t_name);
+  // You just need to travesal the tree and push the right treeNode printer into
+  // a vector
+  std::vector<treeNode*> searchPersonByFilter(
+      std::function<bool(const treeNode*)> filter);
+  int brotherNumber(const std::string t_name);
+  // it's a little hard that the node is not the first son of his father
+  treeNode* findFather(const std::string t_name);
+
+  int getLivePersonNum() const;
+  int getAllPersonNum() const;
+
+  // if we have time, we can use nodejs and construct a web diplay
+  void displayTree() const;
+
+ private:
+  bool sync() const;
+  std::string m_family_tree_name;
+  int m_all_person_num;
+  int m_live_person_num;
+  std::string m_data_path;
+};
+#endif
